2d77e31d470d2241383e91034f5ee4731627de57
sanchitadevi <thesanchitadevi@gmail.com>
2023-06-06 20:58:41 +0600

    theme confiq

--- src/layouts/App/Menu/index.tsx
+++ src/layouts/App/Menu/index.tsx
@@ -7,7 +7,7 @@ import {
 	DialogTitle,
 	Icon,
 } from "@mui/material";
-import { Input } from "antd";
+import { FloatButton, Input } from "antd";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -27,7 +27,7 @@ const AppMenu: React.FC<{ open: boolean; onClose: () => void }> = ({
   }[] = [
     {
       icon: <Iconify icon={"solar:home-angle-bold-duotone"} />,
-      label: "Dashboard",
+      label: "Projects",
       to: "",
     },
     {
@@ -40,21 +40,6 @@ const AppMenu: React.FC<{ open: boolean; onClose: () => void }> = ({
       label: "Testimonial",
       to: "testimonials",
     },
-    // {
-    // 	icon: <Iconify icon={"solar:map-arrow-down-bold-duotone"} />,
-    // 	label: "Map",
-    // 	to: "map",
-    // },
-    // {
-    // 	icon: <Iconify icon={"solar:user-bold-duotone"} />,
-    // 	label: "Critics",
-    // 	to: "critics",
-    // },
-    // {
-    // 	icon: <Iconify icon={"solar:devices-bold-duotone"} />,
-    // 	label: "Platforms",
-    // 	to: "platforms",
-    // },
     {
       icon: <Iconify icon={"solar:shield-user-bold-duotone"} />,
       label: "Moderators",
@@ -83,56 +68,56 @@ const AppMenu: React.FC<{ open: boolean; onClose: () => void }> = ({
 	}, [open]);
 
 	return (
-		<Dialog
-			open={open}
-			onClose={onClose}
-			scroll="body"
-			PaperProps={{
-				sx: {
-					background: "transparent",
-					boxShadow: 0,
-				},
-			}}
-			BackdropProps={{
-				className: "backdrop-blur-lg",
-			}}
-		>
-			<DialogTitle className="sticky">
-				<Input
-					bordered={false}
-					placeholder="Search Page.."
-					className="bg-slate-300 bg-opacity-10 text-xl p-4 px-7 font-bold rounded-full mb-9"
-					size="large"
-					allowClear
-					value={search}
-					onChange={(e) => onSearch(e.target.value)}
-				/>
-			</DialogTitle>
-			<DialogContent className="bg-transparent grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
-				{locations?.map?.((l) =>
-					l.label.toLowerCase().includes(search?.toLowerCase()) ? (
-						<Button
-							className="flex-col items-center gap-2 p-4 px-7"
-							color={l?.color || "inherit"}
-							key={l.label}
-							onClick={() => {
-								if (l.to !== undefined) navigate(`/app/${l.to}`);
+    <Dialog
+      open={open}
+      onClose={onClose}
+      scroll="body"
+      PaperProps={{
+        sx: {
+          background: "transparent",
+          boxShadow: 0,
+        },
+      }}
+      BackdropProps={{
+        className: "backdrop-blur-lg",
+      }}
+    >
+      <DialogTitle className="sticky">
+        <Input
+          bordered={false}
+          placeholder="Search Page.."
+          className="bg-slate-300 bg-opacity-10 text-xl p-4 px-7 font-bold rounded-full mb-9"
+          size="large"
+          allowClear
+          value={search}
+          onChange={(e) => onSearch(e.target.value)}
+        />
+      </DialogTitle>
+      <DialogContent className="bg-transparent grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
+        {locations?.map?.((l) =>
+          l.label.toLowerCase().includes(search?.toLowerCase()) ? (
+            <Button
+              className="flex-col items-center gap-2 p-4 px-7"
+              color={l?.color || "inherit"}
+              key={l.label}
+              onClick={() => {
+                if (l.to !== undefined) navigate(`/app/${l.to}`);
 
-								if (l.func) l.func();
+                if (l.func) l.func();
 
-								onClose();
-							}}
-						>
-							<Icon className="text-4xl">{l.icon}</Icon>
-							<div>{l.label}</div>
-						</Button>
-					) : (
-						<></>
-					)
-				)}
-			</DialogContent>
-		</Dialog>
-	);
+                onClose();
+              }}
+            >
+              <Icon className="text-4xl">{l.icon}</Icon>
+              <div>{l.label}</div>
+            </Button>
+          ) : (
+            <></>
+          )
+        )}
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default AppMenu;
--- src/layouts/App/index.tsx
+++ src/layouts/App/index.tsx
@@ -5,40 +5,36 @@ import React, { lazy } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import AppMenu from "./Menu";
 import { Container, IconButton } from "@mui/material";
+import { FloatButton } from "antd";
+import { CustomerServiceOutlined, CommentOutlined } from "@ant-design/icons";
 
 const AppHeader = lazy(() => import("./Header"));
 const AppFooter = lazy(() => import("./Footer"));
 
 const AppLayout: React.FC = () => {
-	const location = useLocation();
-	const auth = useAuth();
-	const { state, toggleState } = useToggle(false);
+  const location = useLocation();
+  const auth = useAuth();
+  const { state, toggleState } = useToggle(false);
 
-	return auth.isLoggedIn ? (
-		<>
-			<AppHeader />
-			<Container>
-				<Outlet />
-			</Container>
-			<AppFooter />
+  return auth.isLoggedIn ? (
+    <>
+      <AppHeader />
+      <Container>
+        <Outlet />
+      </Container>
+      <AppFooter />
 
-			<IconButton
-				className="fixed bottom-3 right-4 rounded w-20 backdrop-blur shadow-black drop-shadow-lg"
-				onClick={toggleState}
-			>
-				<Iconify
-					icon={"gg:menu-grid-r"}
-					className="text-5xl h-16"
-				/>
-			</IconButton>
-			<AppMenu
-				open={state}
-				onClose={toggleState}
-			/>
-		</>
-	) : (
-		<Navigate to={`/?to=${location.pathname}`} />
-	);
+      <IconButton
+        className="fixed bottom-3 right-4 rounded w-20 backdrop-blur "
+        onClick={toggleState}
+      >
+        <Iconify icon={"gg:menu-grid-r"} className="text-5xl h-16" />
+      </IconButton>
+      <AppMenu open={state} onClose={toggleState} />
+    </>
+  ) : (
+    <Navigate to={`/?to=${location.pathname}`} />
+  );
 };
 
 export default AppLayout;
